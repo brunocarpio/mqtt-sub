8dc0a13bd531d12c39b860a352501ed2198215ac
Bruno Carpio <bcarpio181@outlook.es>
2021-07-13 01:51:03 +0000

    Dec class add

--- mqtt_sub.py
+++ mqtt_sub.py
@@ -11,80 +11,49 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 from datetime import datetime
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument('--host', '-h', required=True)
-parser.add_argument('--topic', '-t', required=True)
-parser.add_argument('--attributes', '-at', required=True)
-parser.add_argument('--client_id', '-i')
-args = parser.parse_args()
+class Dec():
+    def __init__(self, groupObj):
+        self.util = SecretUtil(groupObj, verbose=False)
+        self.group = groupObj
 
-mqttBroker = args.host
-topic = args.topic
-attributes = args.attributes
-client = mqtt.Client(args.client_id)
+    def cpabe_decrypt(self, pk, sk, ct):
+        policy = self.util.createPolicy(ct['policy'])
+        pruned_list = self.util.prune(policy, sk['S'])
+        if pruned_list == False:
+            return False
+        z = self.util.getCoefficients(policy)
+        A = 1
+        for i in pruned_list:
+            j = i.getAttributeAndIndex(); k = i.getAttribute()
+            A *= ( pair(ct['Cy'][j], sk['Dj'][k]) / pair(sk['Djp'][k], ct['Cyp'][j]) ) ** z[j]
+        return ct['C_tilde'] / (pair(ct['C'], sk['D']) / A)
 
-PORT = '8000'
-group = PairingGroup('SS512')
-util = SecretUtil(group, verbose=False)
+    def decrypt(self, pk, sk, payload):
+        ct = bytesToObject(payload, self.group)
+        encrypted_aes_key = ct['encrypted_aes_key']
+        aes_key = self.cpabe_decrypt(pk, sk, encrypted_aes_key)
+        if aes_key:
+            aes_key_bytes = self.group.serialize(aes_key)
 
-pk = bytesToObject(requests.get('http://'
-    + mqttBroker
-    + ':'
-    + PORT
-    + '/pk').content, group)
-
-parameters = ''
-attributes_list = attributes.split()
-for i in range(len(attributes_list)):
-    parameters += 'a' + str(i) + '=' + attributes_list[i].upper()
-    if i < len(attributes_list) - 1:
-        parameters += '&'
-
-sk = bytesToObject(requests.get('http://'
-    + mqttBroker
-    + ':'
-    + PORT
-    + '/keygen?'
-    + parameters).content, group)
-
-def cpabe_decrypt(pk, sk, ct):
-    policy = util.createPolicy(ct['policy'])
-    pruned_list = util.prune(policy, sk['S'])
-    if pruned_list == False:
-        return False
-    z = util.getCoefficients(policy)
-    A = 1
-    for i in pruned_list:
-        j = i.getAttributeAndIndex(); k = i.getAttribute()
-        A *= ( pair(ct['Cy'][j], sk['Dj'][k]) / pair(sk['Djp'][k], ct['Cyp'][j]) ) ** z[j]
-    return ct['C_tilde'] / (pair(ct['C'], sk['D']) / A)
-
-def decrypt(payload):
-    ct_dict = bytesToObject(payload, group)
-    encrypted_aes_key_dict = ct_dict['encrypted_aes_key_dict']
-    aes_key = cpabe_decrypt(pk, sk, encrypted_aes_key_dict)
-    if aes_key:
-        aes_key_bytes = group.serialize(aes_key)
-
-        aes_encryption_dict = ct_dict['aes_encryption_dict']
-        iv = b64decode(aes_encryption_dict['iv'])
-        encrypted_data = b64decode(aes_encryption_dict['encrypted_data'])
-        cipher = AES.new(aes_key_bytes[:16], AES.MODE_CBC, iv)
-        msg = unpad(cipher.decrypt(encrypted_data), AES.block_size)
-        return msg.decode()
-    else:
-        return False
+            encrypted_data = ct['encrypted_data']
+            encrypted_data_k = ['nonce', 'ciphertext', 'tag']
+            encrypted_data = {k:b64decode(encrypted_data[k]) for k in encrypted_data_k}
+            cipher = AES.new(aes_key_bytes[:16], AES.MODE_EAX, nonce=encrypted_data['nonce'])
+            msg = cipher.decrypt_and_verify(encrypted_data['ciphertext'], encrypted_data['tag'])
+            return msg.decode()
+        else:
+            return False
 
 def on_connect(client, userdata, flags, rc):
-    client.subscribe(args.topic)
+    client.subscribe(userdata['topic'])
 
 def on_subscribe(client, userdata, mid, granted_qos):
     print('--------------------------------------------------------------------------------')
-    print(datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ' Suscrito: ' + topic)
+    print(datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ' Suscrito: ' + userdata['topic'])
     print('--------------------------------------------------------------------------------')
 
 def on_message(client, userdata, message):
-    msg = decrypt(message.payload)
+    msg = userdata['dec'].decrypt(userdata['pk'], userdata['sk'], message.payload)
     if msg:
         print('--------------------------------------------------------------------------------')
         print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
@@ -93,6 +62,37 @@ def on_message(client, userdata, message):
         print('--------------------------------------------------------------------------------')
 
 def main():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('--host', '-h', required=True)
+    parser.add_argument('--topic', '-t', required=True)
+    parser.add_argument('--attributes', '-at', required=True)
+    parser.add_argument('--client_id', '-i')
+    args = parser.parse_args()
+
+    mqttBroker = args.host
+    topic = args.topic
+    attributes = args.attributes
+
+    PORT = '8000'
+    group = PairingGroup('SS512')
+    util = SecretUtil(group, verbose=False)
+
+    pk = bytesToObject(requests.get('http://' + mqttBroker
+        + ':' + PORT + '/pk').content, group)
+
+    dec = Dec(group)
+
+    parameters = ''
+    attributes_list = attributes.split()
+    for i in range(len(attributes_list)):
+        parameters += 'a' + str(i) + '=' + attributes_list[i].upper()
+        if i < len(attributes_list) - 1:
+            parameters += '&'
+
+    sk = bytesToObject(requests.get('http://' + mqttBroker
+        + ':' + PORT + '/keygen?' + parameters).content, group)
+
+    client = mqtt.Client(args.client_id, userdata={'dec': dec, 'pk': pk, 'sk': sk, 'topic': topic})
     client.connect(mqttBroker) 
     client.on_connect=on_connect
     client.on_subscribe=on_subscribe
